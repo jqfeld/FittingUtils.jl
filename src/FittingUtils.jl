49f70676eeeeebc7d83951851fb205be69b2ac9d
Jan Kuhfeld <jankuhfeld@plasma-matters.nl>
2026-07-29 11:14:34 +0200

    export interface

--- src/FittingUtils.jl
+++ src/FittingUtils.jl
@@ -4,7 +4,10 @@ using Statistics
 import StatsAPI
 
 include("method.jl")
+export TuringJL, LevenbergMarquardt
 include("results.jl")
+export FitResult
 include("fit_model.jl")
+export fit_model
 
 end
